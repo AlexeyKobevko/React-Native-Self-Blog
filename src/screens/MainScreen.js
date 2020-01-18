11bc6db3bdfd6936c98b3d3918367d5f5aeebcd6
Jedi <folco_2000@mail.ru>
2020-01-18 19:41:18 +0300

    complit

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useDispatch, useSelector } from 'react-redux';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 
 import { AppHeaderIcon } from '../components/AppHeaderIcon';
 import { PostList } from '../components/PostList';
 import { loadPosts } from '../store/actions/post';
+import { THEME } from '../theme';
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = post => {
@@ -22,6 +24,15 @@ export const MainScreen = ({ navigation }) => {
   }, [dispatch]);
 
   const allPosts = useSelector(state => state.post.allPosts);
+  const loading = useSelector(state => state.post.loading);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator color={THEME.MAIN_COLOR} />
+      </View>
+    );
+  }
 
   return <PostList data={allPosts} onOpen={openPostHandler} />;
 };
@@ -46,4 +57,12 @@ MainScreen.navigationOptions = ({ navigation }) => ({
       />
     </HeaderButtons>
   ),
+});
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
